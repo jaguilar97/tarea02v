1dcd558604822df6bb6ac12de28e0ed1b6f2c094
jaguilar97 <98626422+jaguilar97@users.noreply.github.com>
2022-06-07 21:05:28 -0600

    Prueba

---
+++ src/main/java/com/tarea02v/Tarea02vApplication.java
@@ -0,0 +1,13 @@
+package com.tarea02v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea02vApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea02vApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea02v/Tarea02vApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea02v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea02vApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
